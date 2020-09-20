c876db225809bbfb4a03cfb611949fa749ef8984
NejlaTurk <71409798+NejlaTurk@users.noreply.github.com>
2020-09-20 21:38:21 +0100

    Add files via upload
    I have done my homework.

---
+++ aboutMySelf.java
@@ -0,0 +1,30 @@
+package aboutMySelf;
+
+public class aboutMySelf {
+
+	public static void main(String[] args) {
+					
+			
+			int householder, age;
+			
+			age=38;
+			householder= 1;
+		
+			String name, name2, city;
+			
+			name="Nejla";
+			name2= "Turk";
+			city= "Northampton";
+			
+			System.out.println();
+			System.out.println("My name is "+name+" "+name2+", I am "+age+" old, I live in "+city+" city, I have "+householder+" family member.") ; 
+			System.out.println();
+			System.out.println("I am already loving java !!!");
+		
+		
+		
+	}
+
+
+	}
+
